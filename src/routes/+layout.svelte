7e641d5445e38a7d2334a4dfebea524c8406d92b
Tim Zadorozhny <tzador@gmail.com>
2025-01-29 04:51:34 +0100

    Some styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,17 +24,17 @@
 </script>
 
 <header
-  class="fixed top-0 right-0 left-0 grid grid-cols-[1fr_auto_1fr] items-center gap-4 bg-stone-800 p-4"
+  class="fixed top-0 right-0 left-0 grid h-16 grid-cols-[1fr_auto_1fr] items-center gap-4 bg-black/50 px-4"
 >
-  <div class="flex items-center justify-start gap-4">
-    <a class="text-xl font-medium" href="/"><h1>xshader.org</h1></a>
+  <div class="flex">
+    <a class="text-xl font-medium" href="/"><h1>xshader.com</h1></a>
   </div>
   <div class="flex items-center gap-4">
     <a href="/">hot</a>
     <a href="/">top</a>
-    <a href="/">latest</a>
-    <a href="/">random</a>
-    <button class="font-medium text-purple-500" onclick={() => create()}>create</button>
+    <a href="/">new</a>
+    <a href="/">rnd</a>
+    <button class="font-medium text-yellow-600" onclick={() => create()}>create</button>
     <a href="/">faq</a>
     <a href="/">about</a>
   </div>
@@ -42,7 +42,10 @@
     {#if user}
       <div class="flex items-center gap-2">
         <a href="/@{user.username}" class="font-medium">@{user.username}</a>
-        <img src={user.image} alt={user.username} class="h-8 w-8 rounded-full" />
+        <div
+          style:background-image={`url(${user.image})`}
+          class="h-8 w-8 rounded-full bg-cover bg-center"
+        ></div>
         <button onclick={() => signOut()}><LogOut /></button>
       </div>
     {:else}
